468e40a81812e7dd001ee0eda5ca2a596f9dc874
Tensae Befekadu <39292585+tensaeb@users.noreply.github.com>
2024-01-30 19:52:47 +0300

    Portfolio pages done 🍾🥂

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,5 +1,20 @@
+import PortfolioItem from "../../components/Portfolio/PortfolioItem";
+import { portfolio } from "../../data";
+import "./portfolio.css";
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className="portfolio section">
+      <h2 className="section_title">
+        My <span>Portfolio</span>
+      </h2>
+      <div className="portfolio_container container grid">
+        {portfolio.map((item) => {
+          return <PortfolioItem key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
