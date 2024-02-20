c8e716d8205afee0dad9da2c1ce3ecd234270484
Tensae Befekadu <39292585+tensaeb@users.noreply.github.com>
2024-02-20 16:04:47 +0300

    Responsive 👍📌

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,13 +3,12 @@ import "https://cdn.lordicon.com/lordicon.js";
 const Loading = () => {
   return (
     <lord-icon
+      className="loading"
       src="https://cdn.lordicon.com/szoiozyr.json"
       trigger="loop"
       stroke="light"
       state="in-reveal"
       style={{
-        width: "300px",
-        height: "300px",
         position: "absolute",
         top: "50%",
         left: "50%",
